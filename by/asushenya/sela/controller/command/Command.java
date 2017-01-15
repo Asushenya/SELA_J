c508e00c24889baeae0322532fae6bcdf264c274
bntj <asuschenya@gmail.com>
2017-01-15 13:31:57 +0300

    now from GetOwerReport returned String

--- by/asushenya/sela/controller/command/Command.java
+++
@@ -1,5 +0,0 @@
-package by.asushenya.sela.controller.command;
-
-public interface Command {
-	public String execute(String request);
-}
--- by/asushenya/sela/controller/command/impl/CommandName.java
+++
@@ -1,5 +0,0 @@
-package by.asushenya.sela.controller.command.impl;
-
-public enum CommandName {
-	REGISTERED_NEW_USER,RENT_EQUIPMENT, ADD_NEW_EQUIPMENT, GET_OWER_REPORT, WRONG_REQUEST
-}
